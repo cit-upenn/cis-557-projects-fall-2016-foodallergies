cf2da10771954403984f8ab99aaed429353f81b5
RexGoGoGo <zixing.rex.li@gmail.com>
2016-11-11 18:00:21 -0500

    added some cucumber test for allergens

--- features/step_definitions/add_allergen_steps.rb
+++ features/step_definitions/add_allergen_steps.rb
@@ -7,14 +7,32 @@ When(/^I add a nonexisted allergen to the database$/) do
 	click_button 'Create Allergen'
 end
 
-Then(/^I should be able to add an allergen$/) do 
+Then(/^I should be able to add an allergen$/) do
 	assert page.has_content?("Allergen was successfully created.")
 end
 
-When(/^I add a nonexisted allergen without a name to the database$/) do 
+When(/^I add a nonexisted allergen without a name to the database$/) do
 	click_button 'Create Allergen'
 end
 
-Then(/^I should be able to see some errors on the page$/) do 
+Then(/^I should be able to see some errors on the page$/) do
 	assert page.has_content?("Name can't be blank")
+end
+
+When(/^I add an existing allergen to the database$/) do
+	fill_in 'Name', :with => "Egg"
+	click_button 'Create Allergen'
+end
+
+Then(/^I should be able to see some errors for adding a duplicate$/) do
+	assert page.has_content?("Name has already been taken")
+end
+
+When(/^I add a new allergen whose first letter is not capitalized to the database$/) do
+	fill_in 'Name', :with => "seafood"
+	click_button 'Create Allergen'
+end
+
+Then(/^I should be able to see some errors for adding these bad format entry$/) do
+	assert page.has_content?("Name must start with upper case")
 end
