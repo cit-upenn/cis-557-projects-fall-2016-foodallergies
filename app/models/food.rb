07a366f5959d75db631ac3ba391a8aaa2e621b8e
RexGoGoGo <zixing.rex.li@gmail.com>
2016-11-11 16:57:01 -0500

    add validation for capitalization of names for food, ingredient, and allergen.

--- app/models/food.rb
+++ app/models/food.rb
@@ -10,7 +10,12 @@
 #
 
 class Food < ActiveRecord::Base
-	validates :name, :presence => true, :uniqueness => true
+	validates :name, :presence => true
+	validates :product_barcode, :uniqueness =>true
 	has_and_belongs_to_many :ingredients
 	has_many :allergens , through: :ingredients
+
+	validates_each :name do |record, attr, value|
+    record.errors.add(attr, 'must start with upper case') if value =~ /\A[[:lower:]]/
+  end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -12,4 +12,7 @@ class Ingredient < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   has_and_belongs_to_many :foods
   has_and_belongs_to_many :allergens
+  validates_each :name do |record, attr, value|
+    record.errors.add(attr, 'must start with upper case') if value =~ /\A[[:lower:]]/
+  end
 end
