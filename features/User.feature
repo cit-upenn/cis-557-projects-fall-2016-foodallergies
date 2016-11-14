df2d2ed4c499bb7ab457fab00b9ed16fe3364fe6
Yanssie <gaoyanne@gmail.com>
2016-11-14 12:07:15 -0500

    update user cucumber

--- features/User.feature
+++ features/User.feature
@@ -15,3 +15,9 @@ Feature: User sign up page
 		When I add a new user without email field
 		Then show me the page
 		Then I can't create an user without email
+
+	Scenario: Add an user when password does not match
+		Given I'm on the user sign up page
+		When I add a new user but password does not match
+		Then show me the page
+		Then I can't create an user when password does not match
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,24 +1,35 @@
 Given (/^I'm on the user sign up page$/) do
-	visit(new_user_session_path)
+	visit(new_user_registration_path)
 end
 
 When (/^I add a new user$/) do
 	fill_in 'Email', :with => "mary@seas.upenn.edu"
 	fill_in 'Password', :with => "12345678"
-	fill_in 'Password', :with => "12345678"
+	fill_in 'Password confirmation', :with => "12345678"
 	click_button 'Sign up'
 end
 
 Then (/^I should be able to see the homepage$/) do
-	assert page.has_content?("Welcome! You have signed up successfully.")
+	assert page.has_content?("Welcome!")
 end
 
 When (/^I add a new user without email field$/) do
 	fill_in 'Password', :with => "12345678"
-	fill_in 'Password', :with => "12345678"
+	fill_in 'Password confirmation', :with => "12345678"
 	click_button 'Sign up'
 end
 
 Then (/^I can't create an user without email$/) do
 	assert page.has_content?("Email can't be blank")
+end
+
+When (/^I add a new user but password does not match$/) do
+	fill_in 'Email', :with => "mary@seas.upenn.edu"
+	fill_in 'Password', :with => "12345678"
+	fill_in 'Password confirmation', :with => "1234"
+	click_button 'Sign up'
+end
+
+Then (/^I can't create an user when password does not match$/) do
+	assert page.has_content?("Password confirmation doesn't match Password")
 end
