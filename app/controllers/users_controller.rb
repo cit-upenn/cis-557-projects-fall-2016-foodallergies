3526ebcc921a58b4aa2c83bcb0aba0ada5165182
Yanssie <gaoyanne@gmail.com>
2016-11-26 21:59:53 -0500

    user allergens settings and user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+	
+    def show
+    end
+
+	def settings
+		@user = User.find(params[:id])
+		@name = User.find(params[:id]).username
+	end
+
+	def update
+	  @user = User.find(params[:id])
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to controller: 'users', action: 'settings', id: current_user.id, 
+          	notice: 'User settings was successfully updated.'}
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+      private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = user.find(params[:id])
+    end
+    def user_params
+    	params.require(:user).permit(
+    		:username,
+    		:id,
+    		:email,
+    		{:allergen_ids => []}
+    	)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -94,6 +94,11 @@ Rails.application.routes.draw do
   get 'static_page/help'
 
   devise_for :users, :controllers => { sessions: 'users/sessions', :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :users do
+    member do
+      get 'settings'
+    end
+  end
 
   resources :ingredients
 
